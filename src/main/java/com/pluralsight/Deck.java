088fd0d375df05a09bfbbf1a5f1ada5f540e5f03
Billngoga <ngogaabill@example.com>
2025-10-24 14:01:47 -0500

    created Card Class

---
+++ src/main/java/com/pluralsight/Deck.java
@@ -0,0 +1,7 @@
+package com.pluralsight;
+
+import java.util.ArrayList;
+
+public class Deck {
+    private ArrayList<Card> cards = new ArrayList<>();
+ }
---
+++ src/main/java/com/pluralsight/Main.java
@@ -0,0 +1,21 @@
+package com.pluralsight;
+
+import java.util.Scanner;
+
+public class Main {
+    static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        System.out.println("Enter Your Name Player1:");
+        String playerName1 = scanner.nextLine();
+        System.out.println("Enter Your Name Player2:");
+        String playerName2 = scanner.nextLine();
+
+        startGame();
+
+    }
+
+    private static void startGame() {
+
+    }
+}
